bfd95057a9589f3e7471932ee4ac132718338e1d
gracekly <gracekly5@gmail.com>
2024-07-03 11:28:10 -0300

    funcoes

--- sistema-bancario/sistema-bancario.py
+++ sistema-bancario/sistema-bancario.py
@@ -1,3 +1,62 @@
+def sacar(*, LIMITE_SAQUES, saldo, numero_saques, extrato, limite ):
+    if (numero_saques < LIMITE_SAQUES):
+            sacar = float(input("Digite o valor desejado para saque: "))
+
+            if (saldo - sacar) > 0:
+
+                if sacar <= limite:
+
+                    saldo = saldo - sacar
+                    print("Saque realizado com sucesso!")
+                    numero_saques +=1
+                    extrato += f"Saque: R$ {sacar:.2f}\n"
+
+                else:
+                    print(f"O limite de valor por saque é  R$ {limite}.")
+                    
+            else:
+                print("Valor indisponível para saque.")
+    else:
+            print("O limite de saques diário foi atingido.")
+
+    return saldo, extrato
+
+
+def setDeposito(saldo, extrato):
+    depositar = float(input("Digite o valor desejado para depósito: "))
+    if depositar>0:
+        saldo = saldo+depositar
+        extrato += f"Depósito: R$ {depositar}\n"
+    else:
+        print("Valor inválido!")
+
+    return saldo, extrato
+
+
+def getExtrato(saldo, *, transacoes):
+    print("Não foram realizadas movimentações." if not transacoes else transacoes) # se extrato for uma string vazia, executa print; se nao for vazia, retorna extrato
+    print(f"\nSaldo: R$ {saldo:.2f}")
+
+     
+def criarUsuario(lista_usuarios):
+    nome = input("Insira seu nome: ")
+    data_nascimento = input("Insira sua data de nascimento: ")
+    cpf = int(input("Digite seu CPF: "))
+    print("Insira seu endereço: ")
+    logradouro = input("Logradouro: ")
+    numero = int(input("Numero: "))
+    bairro = input("Bairro: ")
+    cidade = input("Cidade/UF: ")
+
+
+    if cpf in lista_usuarios:
+        print("CPF já cadastrado.")
+        return
+
+    lista_usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereço": {"logradouro": logradouro, "numero": numero, "bairro": bairro, "cidade": cidade}})
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+
 menu = """
 ----- MENU -----
 
@@ -20,43 +79,24 @@ while True:
 
     if opcao == "1": # depósito----------------------------------------------------------------------
         print("Depósito")
-
-        depositar = float(input("Digite o valor desejado para depósito: "))
-        if depositar>0:
-            saldo = saldo+depositar
-            extrato += f"Depósito: R$ {depositar}\n"
-        else:
-            print("Valor inválido!")
+        saldo, extrato = setDeposito(saldo, extrato)
+        print(f"\nSaldo atual = R$ {saldo: .2f}")
+        print(f"\nExtrato: \n{extrato}")
+       
         
 
     elif opcao == "2": # saque----------------------------------------------------------------------
         print("Saque\n")
 
+        saldo1, extrato1 = sacar(LIMITE_SAQUES=LIMITE_SAQUES, saldo= saldo, numero_saques= numero_saques, extrato=extrato, limite=limite)
+        print(f"\nSaldo atual = R$ {saldo1: .2f}")
+        print(f"\nExtrato: \n{extrato}")
     
-        if (numero_saques < LIMITE_SAQUES):
-            sacar = float(input("Digite o valor desejado para saque: "))
-
-            if (saldo - sacar) > 0:
-
-                if sacar <= limite:
-
-                    saldo = saldo - sacar
-                    print("Saque realizado com sucesso!")
-                    numero_saques +=1
-                    extrato += f"Saque: R$ {sacar:.2f}\n"
-
-                else:
-                    print(f"O limite de valor por saque é  R$ {limite}.")
-            else:
-                print("Valor indisponível para saque.")
-        else:
-            print("O limite de saques diário foi atingido.")
 
         
         
     elif opcao == "3": #extrato-----------------------------------------------------------------
-        print("Não foram realizadas movimentações." if not extrato else extrato) # se extrato for uma string vazia, executa print; se nao for vazia, retorna extrato
-        print(f"\nSaldo: R$ {saldo:.2f}")
+       getExtrato(saldo, transacoes=extrato)
 
 
     elif opcao == "4": #sair----------------------------------------------------------------------
